171f7291d9c4bb36bcee9d14f5f16bb9da3f1378
Ulrik Sandberg <ulrik.sandberg@gmail.com>
2016-11-22 23:20:17 +0100

    ENTERPRISE-304: Now handling JIRA custom field for story points

--- src/codescene_enterprise_pm_jira/jira.clj
+++ src/codescene_enterprise_pm_jira/jira.clj
@@ -2,7 +2,8 @@
   (:require [clj-http.client :as client]
             [cheshire.core :as json]
             [slingshot.slingshot :refer [try+ throw+]]
-            [taoensso.timbre :as log]))
+            [taoensso.timbre :as log]
+            [clojure.string :as string]))
 
 (def ^:const ^:private rest-api-path
   "/rest/api/latest")
@@ -22,7 +23,7 @@
 
 (defn- combine-labels-with-issue-type
   [labels issue-type]
-  {:pre  [(set? labels) (or (nil? issue-type) (string? issue-type))]}
+  {:pre [(set? labels) (or (nil? issue-type) (string? issue-type))]}
   (if (some? issue-type)
     (clojure.set/union labels #{issue-type})
     labels))
@@ -48,33 +49,36 @@
 (defn find-issues-with-cost
   "Fetches issues from the remote JIRA API. Returns nil when the API calls fail."
   [base-uri username password {:keys [key supported-work-types] :as _project-config} cost-field-name]
-  (try+
-    (map (partial jira-issue->db-format cost-field-name (set supported-work-types))
-         (get-paged-data base-uri {:basic-auth   [username password]
-                                   :accept       :json
-                                   :query-params {:jql (format "project=%s and %s!=EMPTY"
-                                                               key
-                                                               cost-field-name)}}))
-    (catch [:status 400] {:keys [body]}
-      (log/errorf "Could not find issues in project %s: %s" key body))
+  (let [search-field-name (if (string/starts-with? cost-field-name "customfield_")
+                            (str "cf[" (second (string/split cost-field-name #"_")) "]")
+                            cost-field-name)]
+    (try+
+      (map (partial jira-issue->db-format cost-field-name (set supported-work-types))
+           (get-paged-data base-uri {:basic-auth   [username password]
+                                     :accept       :json
+                                     :query-params {:jql (format "project=%s and %s!=EMPTY"
+                                                                 key
+                                                                 search-field-name)}}))
+      (catch [:status 400] {:keys [body]}
+        (log/errorf "Could not find issues in project %s: %s" key body))
 
-    (catch [:status 401] _
-      (log/errorf "Authentication failed when fetching issues for project %s." key))
+      (catch [:status 401] _
+        (log/errorf "Authentication failed when fetching issues for project %s." key))
 
-    (catch [:status 403] {:keys [body headers]}
-      (if (= "AUTHENTICATION_DENIED" (get headers "X-Seraph-LoginReason"))
-        (log/error
-          (str "JIRA denied authentication when fetching issues for project " key
-               ". This usually means that JIRA's CAPTCHA feature has been"
-               " triggered for the authenticating user. Log in using the JIRA"
-               " web site and then try again."))
-        (log/errorf "Unauthorized when fetching issues for project %s." key)))
+      (catch [:status 403] {:keys [body headers]}
+        (if (= "AUTHENTICATION_DENIED" (get headers "X-Seraph-LoginReason"))
+          (log/error
+            (str "JIRA denied authentication when fetching issues for project " key
+                 ". This usually means that JIRA's CAPTCHA feature has been"
+                 " triggered for the authenticating user. Log in using the JIRA"
+                 " web site and then try again."))
+          (log/errorf "Unauthorized when fetching issues for project %s." key)))
 
-    (catch java.net.SocketTimeoutException e
-      (log/errorf "Socket timeout when fetching issues for project %s: %s" key e))
+      (catch java.net.SocketTimeoutException e
+        (log/errorf "Socket timeout when fetching issues for project %s: %s" key e))
 
-    (catch Object _
-      (log/errorf "Unexpected error when fetching issues for project %s: %s"
-                  key
-                  (:throwable &throw-context))
-      (throw+))))
+      (catch Object _
+        (log/errorf "Unexpected error when fetching issues for project %s: %s"
+                    key
+                    (:throwable &throw-context))
+        (throw+)))))
