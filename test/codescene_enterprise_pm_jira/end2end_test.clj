7d88e04e5b3d9518bee6f94c23102279c34c782d
Juraj Martinka <jumarko@gmail.com>
2017-05-30 09:56:35 +0200

    WIP: jira regression test - check expected project json loaded from external golden copy file

--- test/codescene_enterprise_pm_jira/end2end_test.clj
+++ test/codescene_enterprise_pm_jira/end2end_test.clj
@@ -1,18 +1,27 @@
 (ns codescene-enterprise-pm-jira.end2end-test
-  (:require [clojure.test :refer :all]
-            [codescene-enterprise-pm-jira.handler :refer [app init-app]]
+  (:require [clojure
+             [string :refer [replace]]
+             [test :refer :all]]
+            [codescene-enterprise-pm-jira
+             [db :as db]
+             [handler :refer [app init-app]]]
             [ring.mock.request :as mock]
-            [ring.util.codec :refer [base64-encode]]
-            [codescene-enterprise-pm-jira.db :as db]))
+            [ring.util.codec :refer [base64-encode]]))
 
 ;; TODO: document env variables?
 ;; - check that DB directory doesn't exist before running test
 
+(defn- read-json [filename]
+  (-> (str "test/codescene_enterprise_pm_jira/jira_golden_copy/" filename)
+      slurp
+      (replace #"\s+" "")))
+
+(def cse-project-json (read-json "cse2.json"))
+
 (def cse-project {:key                  "CSE2"
-                  :cost-unit            {:type "minutes"}
-                  :cost-field           "timeoriginalestimate"
-                  :supported-work-types ["Bug" "Feature" "Refactoring" "Documentation" "Epic" "UI/UX" "Cool+_Stuff_-_Good_to_Have"
-                                         "Must_Have" "Needs_research"]
+                  :cost-unit            {:type "minutes"} :cost-field "timeoriginalestimate"
+                  :supported-work-types ["Bug" "Feature" "Refactoring" "Documentation" "Epic" "UI/UX"
+                                         "Cool+_Stuff_-_Good_to_Have" "Must_Have" "Needs_research"]
                   :ticket-id-pattern    "CSE2-(\\d+)"})
 
 (def ti-project {:key                  "TI"
@@ -21,15 +30,6 @@
                  :supported-work-types ["Bug" "Improvement"]
                  :ticket-id-pattern    "TI-(\\d+)"})
 
-(def test-app
-  (do (db/init)
-      (init-app {:sync {:hour-interval 1}
-                 :auth {:jira {:username (System/getenv "JIRA_USERNAME")
-                               :password (System/getenv "JIRA_PASSWORD")}
-                        :service {:username "testuser"
-                                  :password "testpassword"}}
-                 :projects [cse-project ti-project]})))
-
 (defn- add-auth-header [request]
   (mock/header request
                "Authorization"
@@ -37,11 +37,32 @@
                     (base64-encode
                      (.getBytes "testuser:testpassword" "UTF-8")))))
 
+(defn- request [ring-request]
+  (app (-> ring-request
+           add-auth-header)))
+
+(defn- configure-service [test-fn]
+  (db/init)
+  (init-app {:sync     {:hour-interval 1}
+             :auth     {:jira    {:username (System/getenv "JIRA_USERNAME")
+                                  :password (System/getenv "JIRA_PASSWORD")
+                                  :base-uri "http://jira-integration.codescene.io"}
+                        :service {:username "testuser"
+                                  :password "testpassword"}}
+             :projects [cse-project ti-project]})
+  (test-fn))
+
+(defn- sync-projects-from-jira [test-fn]
+  (doseq [project [cse-project ti-project]]
+    (request (mock/request :post "/sync/force" {:project-key (:key project)})))
+  (test-fn))
+
+(use-fixtures :each (join-fixtures [configure-service sync-projects-from-jira]))
+
 (deftest test-app
   (testing "project stats"
-    (let [response (app (-> (mock/request :get "/api/1/projects/CSE2")
-                            add-auth-header))
-          _ (println response)]
+    (let [response (request (mock/request :get "/api/1/projects/CSE2"))]
       (is (= (:status response) 200))
-      (is (clojure.string/includes? (get-in response [:headers "Content-Type"]) "text/html")))))
+      (is (clojure.string/includes? (get-in response [:headers "Content-Type"]) "application/json"))
+      (is (= cse-project-json (:body response))))))
 
